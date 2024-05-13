ec7cf1fa7006dd554f0292fc7496ab84fbe8e916
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-05-13 23:42:32 +0500

    evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage1dataingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage2preparebasemodel import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage3training import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage4evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -35,6 +36,19 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
